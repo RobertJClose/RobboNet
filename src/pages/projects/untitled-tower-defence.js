c2bcc4c29e761501728cfc74734d81d9006d690c
Robert <88722095+RobertJClose@users.noreply.github.com>
2023-03-12 15:47:26 +0000

    Fix typos and phrasing on Tower Defence page

--- src/pages/projects/untitled-tower-defence.js
+++ src/pages/projects/untitled-tower-defence.js
@@ -6,7 +6,7 @@ import WaypointsSceneShowoff from "./videos/Quick Turret Waypoint Scene View.mp4
 const TowerDefencePage = () => {
   return (
     <Layout pageTitle="RobboNet: Untitled Tower Defence Game">
-            <h1>Quick Turret (Tower Defence Project)</h1>
+      <h1>Quick Turret (Tower Defence Project)</h1>
       <p>
         This last project started as a small and fast project that was only
         meant to show me how much I'd learned. I'd been playing a lot of{" "}
@@ -42,7 +42,8 @@ const TowerDefencePage = () => {
         to navigate the path, but this led to the enemies mundanely following
         each other with ignorance to the branching paths. So I set about
         creating waypoints for them to follow, but this proved to be incredibly
-        tedious. I understand better now that an important part of being a good
+        tedious, and added masses of complexity to the organisation of my
+        project. I understand better now that an important part of being a good
         Unity developer is being able to create not just powerful MonoBehaviour
         components, but also custom Editor behaviour that makes those components
         easy to work with. So I set about creating a powerful waypoint system
@@ -53,12 +54,13 @@ const TowerDefencePage = () => {
         Unity Inspector, the component has a custom list view that gives the
         user not just the data associated with each Waypoint, but simple ways of
         editing that data. The real power of the component is in the Scene view
-        however. I wanted user to be able to use the component entirely within the 
-        Scene view via handles and a simple GUI. From the Scene view GUI each waypoint
-        can be renamed, moved about, and have its connections edited. Handles in the 
-        Scene view provide the easier way of manipulating waypoints and their connections 
-        however. There are hotkeys to make editing really fast, and the component is 
-        totally integrated with the Unity Editor's Undo system.
+        however. I wanted a user to be able to use the component entirely within
+        the Scene view via handles and a simple GUI. From the Scene view GUI
+        each waypoint can be renamed, moved about, and have its connections
+        edited. Handles in the Scene view also provide an easier way of
+        manipulating waypoints and their connections. There are hotkeys to make
+        editing really fast, and the component is totally integrated with the
+        Unity Editor's Undo system.
       </p>
       <video autoPlay controls loop muted width="720">
         <source src={WaypointsSceneShowoff} type="video/mp4" />
