29ba5f5a8c09416aaf84c5847fed16b5e7a1fc37
Robert <88722095+RobertJClose@users.noreply.github.com>
2023-03-14 09:41:22 +0000

    Add bio to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,15 +27,36 @@ const projectsData = [
 const IndexPage = ({ data }) => {
   return (
     <Layout pageTitle="RobboNet">
+      <h1>Bio:</h1>
       <p>
-        Hello there! <br></br>
-        This simple site has all the things I've been working on.
+        Hello there! My name is Robbie.<br></br>
+        My aim is to start a career in the computer games industry, and this
+        website presents my portfolio of projects. You may also find more on{" "}
+        <a href="https://github.com/RobertJClose" target="_blank">
+          my GitHub.
+        </a>{" "}
+        After graduating with an MSci in theoretical physics from the University
+        of Nottingham, and then being waylaid by the pandemic, I started
+        teaching myself programming through the Unity game engine.
       </p>
       <p>
-        My{" "}
-        <a href="https://github.com/RobertJClose" target="_blank">
-          GitHub.
-        </a>
+        I'd say that my greatest asset is that I am very patient and ruthlessly
+        tenacious. It is these two qualities that I try to leverage to win at
+        games, and they have been indispensable to me as I've taught myself
+        programming. I like to learn with a systematic and layered approach that
+        starts from fundamentals, before I get to the more immediately rewarding
+        tutorials that tackle some prestigious obstacle. I feel that my approach
+        gives my progress and eventual success a sense of inevitability, and I
+        find it incredibly satisfying watching that process unfold. I use
+        note-taking as part of my learning process, and I've filled ~960 pages
+        of A5 notebooks since starting my programming journey. I have since
+        migrated to Obsidian personal knowledge management software to keep my
+        notes and thoughts organised as I roll towards the start of my career.
+      </p>
+      <p>
+        The website itself was built using the React Javascript library, and
+        deployed using Netlify. The videos on the website were edited with
+        Shotcut.
       </p>
       <ProjectGallery projectsData={projectsData} />
       <video autoPlay controls loop muted width="720">
