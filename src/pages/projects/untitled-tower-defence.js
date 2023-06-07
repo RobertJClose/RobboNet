d34a6ffa16b27da1e3eadd480062ed17580663c9
Robert <88722095+RobertJClose@users.noreply.github.com>
2023-06-07 15:17:36 +0100

    Write up new Quick Turret demo details

--- src/pages/projects/untitled-tower-defence.js
+++ src/pages/projects/untitled-tower-defence.js
@@ -4,8 +4,10 @@ import Layout from "../../components/layouts/Layout";
 import styled from "styled-components";
 import WaypointsSceneShowoff from "./videos/Quick Turret Waypoint Scene View.mp4"
 import { Unity, useUnityContext } from "react-unity-webgl";
-import { globalHistory } from "@reach/router"
-
+import { globalHistory } from "@reach/router";
+import BlockedCriticalDamageText from "./images/BlockedCriticalDamageText.png";
+import SubtargetsImage from "./images/SubtargetGizmos.png";
+import AutoloaderImage from "./images/AutoloaderComponent.png";
 
 const TowerDefencePage = () => {
   const { unityProvider, isLoaded, unload } = new useUnityContext({
@@ -46,14 +48,15 @@ const TowerDefencePage = () => {
         systems in particular drew my attention. I know the game is made in
         Unity, and I'd spotted several things that to me were symptomatic of the
         game's code being well structured.
-        <br/>
-        Risk Of Rain's damage system can handle a
-        plethora of interacting damage types being dealt to players, turrets,
-        enemies, and environment features all at once in massive quantities.
-        Furthermore, success in Risk Of Rain 2 involves combining lots of items
-        and player abilities in satisfying synergy. Bloons, on the other hand,
-        requires synergy between the placement of different turret types and the
-        path the balloons are following. This pushed me to build this demo:
+      </p>
+      <p>
+        Risk Of Rain's damage system can handle a plethora of interacting damage
+        types being dealt to players, turrets, enemies, and environment features
+        all at once in massive quantities. Furthermore, success in Risk Of Rain
+        2 involves combining lots of items and player abilities in satisfying
+        synergy. Bloons, on the other hand, requires synergy between the
+        placement of different turret types and the path the balloons are
+        following. This pushed me to build this demo:
       </p>
       <Unity
         unityProvider={unityProvider}
@@ -63,37 +66,279 @@ const TowerDefencePage = () => {
           visibility: isLoaded ? "visible" : "hidden",
         }}
       />
-      <h2>The Waypoints Component</h2>
+      <h2>The Systems</h2>
       <p>
-        The reason for including this project here is that working on this
-        project has seriously levelled up my Unity skills. After coming up with
-        the above demo, I then created a path in Blender for the enemies to
-        follow. At first I used Unity's NavMesh system to tell the enemies how
-        to navigate the path, but this led to the enemies mundanely following
-        each other with ignorance to the branching paths. So I set about
-        creating waypoints for them to follow, but this proved to be incredibly
-        tedious, and added masses of complexity to the organisation of my
-        project. I understand better now that an important part of being a good
-        Unity developer is being able to create not just powerful MonoBehaviour
-        components, but also custom Editor behaviour that makes those components
-        easy to work with. So I set about creating a powerful waypoint system
-        that is well integrated into the Unity Editor.
+        After struggling with scaling complexity in the KSP Companion Prototype,
+        I focused this time on creating modular systems that work well with
+        Unity's GameObject/Monobehaviour component system. This resulted in the
+        following systems:
+        <ul>
+          <li>
+            <a href="#WaypointsSystem">The Waypoints System</a>
+          </li>
+          <li>
+            <a href="#DamageSystem">The Damage System</a>
+          </li>
+          <li>
+            <a href="#TargetingSystem">The Targeting System</a>
+          </li>
+          <li>
+            <a href="#TurretComponents">The Turret Components</a>
+          </li>
+        </ul>
+        The Waypoints, Damage, and Targeting systems are all mutually
+        independent of one another, and may be readily reused in another
+        project. The turrets and enemies in the demo then use these systems to
+        implement their behaviour, along with some bespoke components.
       </p>
-      <video autoPlay controls loop muted width="720">
+      <h3 id="WaypointsSystem">The Waypoints System</h3>
+      <p>
+        This was the first system to be built. This system is used by the
+        enemies to navigate around the path. Originally I had a more complex
+        path with many branches, and this system was up to the task of guiding
+        enemies towards the end. The Waypoints System has four classes, two of
+        which are MonoBehaviour components:
+        <ul>
+          <li>Waypoint</li>
+          <li>WaypointConnection</li>
+          <li>Waypoints (MonoBehaviour)</li>
+          <li>WaypointsNavigator (MonoBehaviour)</li>
+        </ul>
+        The Waypoints component stores a list of Waypoint objects, each of which
+        has a Vector3 containing its position in the 3D Scene. Each Waypoint
+        also keeps a list of WaypointConnection objects, each of which can point
+        to another Waypoint. In this way, a directed graph (digraph) is formed.
+      </p>
+      <p>
+        The Waypoints component the integrates said digraph with a Unity
+        GameObject, while a WaypointsNavigator component offers a simple API for
+        navigating around a graph of Waypoints. The Waypoint and
+        WaypointConnection classes are in theory extendable with weights and
+        more, enabling custom and complex navigation of the waypoints - I did
+        envision 'linelike' and 'arealike' Waypoints to coexist with the
+        pointlike Waypoints, but in reality implementing this wasn't within the
+        scope of the project.
+      </p>
+      <p>
+        I created powerful custom Editor tools for manipulating the Waypoints
+        graph. In the Unity Inspector you can change the name, position,
+        connections, or delete a Waypoint. The Scene view has custom behaviour
+        that's even more powerful however. Via handles you can relocate
+        waypoints, append new waypoints, add and remove connections, and split
+        connections in half. The Scene view also features a custom GUI can be
+        expanded or hidden, and thanks to some hotkeys the Scene view workflow
+        is really smooth:
+      </p>
+      <video autoPlay controls loop muted width="540">
         <source src={WaypointsSceneShowoff} type="video/mp4" />
       </video>
+      <h3 id="DamageSystem">The Damage System (and DamageText subsystem)</h3>
       <p>
-        The result is the Waypoints component, and its custom Editor. In the
-        Unity Inspector, the component has a custom list view that gives the
-        user not just the data associated with each Waypoint, but simple ways of
-        editing that data. The real power of the component is in the Scene view
-        however. I wanted a user to be able to use the component entirely within
-        the Scene view via handles and a simple GUI. From the Scene view GUI
-        each waypoint can be renamed, moved about, and have its connections
-        edited. Handles in the Scene view also provide an easier way of
-        manipulating waypoints and their connections. There are hotkeys to make
-        editing really fast, and the component is totally integrated with the
-        Unity Editor's Undo system.
+        To handle the dealing of damage I then created the Damage System. There
+        are a few structs and classes composing the Damage System:
+        <ul>
+          <li>DamageEffect</li>
+          <li>DamageType</li>
+          <li>Hurtable (MonoBehaviour)</li>
+          <li>HurtableStats (ScriptableObject)</li>
+        </ul>
+        The most important classes are DamageEffect, DamageType and Hurtable.
+        Each DamageEffect instance contains an amount of damage, as well as a
+        DamageType variable that indicates if this instance of damage carries
+        any special effects. DamageType is an enum with the flags attribute,
+        allowing multiple types to be applied at once. DamageEffects can then be
+        applied through the <code>Hurt()</code> method on any Hurtable
+        component.
+      </p>
+      <p>
+        Using the DamageSystem to deal damage is thus very simple: if you have
+        something you want to take damage, give it a Hurtable component; if you
+        have something you want to deal damage, have it create DamageEffect
+        instances and call <code>Hurtable.Hurt()</code>. When you need your
+        other components to react to taking damage (or dying) there are then
+        simple <code>OnHurt</code> and <code>OnDie</code> events that components
+        can subscribe to.
+      </p>
+      <h4>The DamageText Subsystem</h4>
+      <p>
+        To visualise dealt damage I then made the DamageText subsystem:
+        <ul>
+          <li>DamageText (MonoBehaviour)</li>
+          <li>DamageTextFactory (MonoBehaviour)</li>
+          <li>DamageTextSettings (ScriptableObject)</li>
+        </ul>
+        A DamageTextFactory component can be placed on a Hurtable GameObject to
+        produce prefabs with a DamageText component when the <code>OnHurt</code>{" "}
+        event is published. The DamageText component simply handles the tweening
+        of the prefab over its lifetime.
+      </p>
+      <p>
+        The DamageTextFactory checks the DamageType of the incoming damage, and
+        then uses a DamageTextSettings object to decide what settings to give
+        each DamageText instance. The settings contained in a DamageTextSettings
+        object allow for some really nice behaviour, where certian types of
+        damage can take priority and override the cosmetic effects of lower
+        priority effects. For example, its important that players know when
+        their damage is blocked, so dark grey blocked damage has a high
+        priority.
+      </p>
+      <p>
+        With the help of another powerful custom Editor, a DamageTextSettings
+        object allows for some cosmetic options to remain unset by an override,
+        so the final appearance of a DamageText instance can be a mixture that
+        represents several present DamageTypes all at once. An common example of
+        this in the demo is blocked-critical damage - the text is styled as
+        critical damage, except its colour is overridden to dark grey:
+      </p>
+      <img
+        src={BlockedCriticalDamageText}
+        alt="Blocked Critical Damage Text"
+        width="250"
+      />
+      <h3 id="TargetingSystem">The Targeting System</h3>
+      <p>
+        Next we have the Targeting System. This system allows for GameObjects to
+        detect potential targets, prioritise between them, and then select one
+        of several potential subtargets. It uses the following classes:
+        <ul>
+          <li>TargetScanner (MonoBehaviour)</li>
+          <li>Targetable (MonoBehaviour)</li>
+          <li>Subtarget (MonoBehaviour)</li>
+          <li>TargetingTag</li>
+          <li>TagsAsset (ScriptableObject)</li>
+          <li>TagPriorities (ScriptableObject)</li>
+        </ul>
+        The TargetScanner component scans within a given range at a given
+        period, and reports any detected Targetables via an event. If you want a
+        GameObject to be detectable, you simply have to give it a Targetable
+        component.
+      </p>
+      <p>
+        The default behaviour of the TargetScanner is to sort the detected
+        Targetables by their distance to the scanner. However, in order to
+        facilitate target prioritisation I then created a system of tags.
+        Targetables can be given tags as defined in a TagsAsset object. A
+        TagsPriorities object can then be used to sort the Targetables by their
+        tags.
+      </p>
+      <p>
+        To make enemies more interesting, I also created the Subtarget
+        component. Every Targetable has at least one Subtarget that is
+        automatically created on the same GameObject, but more Subtargets can
+        also be created and registered with a Targetable either through the
+        Inspector or the interface of Targetable. The Subtargets can be moved
+        around relative to the origin of the Targetable, and they have their own
+        subset of tags and thus options for prioritisation.
+      </p>
+      <img
+        src={SubtargetsImage}
+        alt="Containozoid Enemy Subtargets"
+        width="550"
+      />
+      <h3 id="TurretComponents">The Turret Components</h3>
+      <p>
+        Finally, I brought these systems together along with some bespoke
+        components to make the turrets. The machine gun, minigun, and sniper
+        turrets all use these components with different variable values to
+        implement their behaviour:
+        <ul>
+          <li>TurretController</li>
+          <li>TargetScanner (Targeting System)</li>
+          <li>AttitudeControlSystem</li>
+          <li>Autoloader</li>
+          <li>RaycastGun</li>
+          <li>BulletTrailFactory</li>
+        </ul>
+        The minigun and sniper also have some of their own components in order
+        to make the barrels of the minigun spin, and to implement the sniper's
+        laser sight.
+      </p>
+      <h4>The Turret Controller</h4>
+      <p>
+        This component acts as a master controller for all the other components
+        on the turrets. By polling the state of the other components, or by
+        subscribing to their events, the turret controller can then dispatch
+        orders to the other components on the turret.
+      </p>
+      <h4>The Target Scanner</h4>
+      <p>
+        This is the TargetScanner component from the Targeting System described
+        above. Its task is to periodically scan for nearby Targetables and
+        publish a prioritised list of them in an event.
+      </p>
+      <h4>The Attitude Control System</h4>
+      <p>
+        Given a particular Subtarget to aim at, this component is responsible
+        for calculating and implementing the correct orientation of the turret's
+        model. I decided to have all three types of turret use the same base
+        structure in their models so that they could have this component in
+        common.
+      </p>
+      <h4>The Autoloader</h4>
+      <p>
+        I really like the idea in{" "}
+        <a href="https://en.wikipedia.org/wiki/War_Thunder" target="_blank">
+          WarThunder
+        </a>{" "}
+        of equipping your aircraft with ammo belts that match the target you are
+        hunting. The ammo belt then has a variety of rounds that are cycled
+        through periodically.
+      </p>
+      <p>
+        The first thing I did to implement this behaviour was to create a
+        ScriptableObject class called AmmoType. Each AmmoType has a name, a
+        colour, an amount of base damage, and then a probability of causing any
+        particular DamageType of the Damage System. The{" "}
+        <code>CreateDamageEffect()</code> method can then be used to create a
+        random DamageEffect based on the probabilities defined by that AmmoType.
+      </p>
+      <p>
+        The Autoloader component is then responsible for maintaining an ammo
+        belt composed of various AmmoTypes. The{" "}
+        <code>DrawChamberedRound()</code> method returns the AmmoType that is
+        current chambered while a reload cooldown starts and the belt advances
+        to the next round. I made sure that null entries in the AmmoBelt can be
+        skipped over until a non-null entry is found - an exception is only
+        thrown in the event that all the rounds in the belt are null.
+      </p>
+      <p>
+        There are also methods for manipulating the size and contents of the
+        belt, peeking at the currently chambered AmmoType or a round at a given
+        index, and resetting the belt back to its first round.
+      </p>
+      <p>
+        Under the hood, the 'ammo belt' is simply a list of AmmoType objects.
+        The default Unity property drawer for this was a poor fit, so as a final
+        touch I made a really nice UI for manipulating the ammo belt in the
+        inspector.
+      </p>
+      <img
+        src={AutoloaderImage}
+        alt="Autoloader Component Custom Inspector"
+        width="450"
+      />
+      <h4>The RaycastGun</h4>
+      <p>
+        This component actually fires at the target. It simply wraps the{" "}
+        <code>Physics.Raycast()</code> method such that a degree of spread is
+        applied to the bullet and the <code>Hurt()</code> method is called on
+        any hit Hurtable.
+      </p>
+      <h4>The Bullet Trail Factory</h4>
+      <p>
+        Finally, this component is responsible for creating and styling the
+        bullet trail leaving the gun barrel. The effect is not very obvious, but
+        if you look closely you can see that the colour of the bullet trail
+        reflects the AmmoType of the fired round.
+      </p>
+      <h2>The Original Demo</h2>
+      <p>
+        I stated at the top that this project had very small and quickly built
+        origins, so here it is. Many of the systems described above are not
+        present in the code of this demo, and I think enthusiastically expanding
+        in scale from this state of affairs would've eventually run into some
+        major issues. Instead I feel I have built something that could much more
+        readily expand to encompass a complete game.
       </p>
     </Layout>
   );
